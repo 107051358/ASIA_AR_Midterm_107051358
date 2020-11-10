7f6b10074dac48fd098e14909d3ee65d866357eb
107051358 <72062978+107051358@users.noreply.github.com>
2020-11-10 13:55:38 +0800

    做到編譯器設定vs2019

--- AR_Midterm_107051358/Assets/Scripts/MenuManager.cs
+++ AR_Midterm_107051358/Assets/Scripts/MenuManager.cs
@@ -1,17 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+   public void StartGame()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    }
+   public void QuitGame()
     {
         
     }
