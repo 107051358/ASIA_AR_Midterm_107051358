c52b3428577e497ffe938891e567b9884dd32e06
107051358 <72062978+107051358@users.noreply.github.com>
2020-11-16 16:17:25 +0800

    完成

--- AR_Midterm_107051358/Assets/Scripts/Player.cs
+++ AR_Midterm_107051358/Assets/Scripts/Player.cs
@@ -1,12 +1,25 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     public GameObject final;
+    public Text textCount;
+    public int count;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        print("碰到傳送門啦");
-        final.SetActive(true);
+       
+        if (collision.name == "傳送門")
+        {
+            final.SetActive(true);
+        }
+        if (collision.tag == "鑽石")
+        {
+            Destroy(collision.gameObject);
+            count ++;
+            textCount.text = "鑽石:" + count;
+        }
+        
     }
 
 }
